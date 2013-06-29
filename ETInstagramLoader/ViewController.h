3f06298e47173253794ee0ec0855f6322296d4e2
DreadlockEugene <dreadlockeugene@gmail.com>
2013-06-29 10:46:24 +0600

    create project

---
+++ ETInstagramLoader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ETInstagramLoader
+//
+//  Created by Eugene Trapeznikov on 6/29/13.
+//  Copyright (c) 2013 Eugene Trapeznikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ETInstagramLoader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ETInstagramLoader
+//
+//  Created by Eugene Trapeznikov on 6/29/13.
+//  Copyright (c) 2013 Eugene Trapeznikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
